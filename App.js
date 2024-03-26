2606941443c3285499018da0e9a7248534502bea
aminofabian <aminofab@gmail.com>
2024-03-26 12:40:13 +0300

    added refreshing

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { SafeAreaView, StyleSheet, Text, View, FlatList, ActivityIndicator } fro
 export default function App() {
   const [postList, setPostList] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
   const fetchData = async (limit = 20) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}`);
@@ -48,6 +49,12 @@ export default function App() {
         ListHeaderComponent={<Text style={styles.headerText}>Post Titles and Content</Text>}
         ListFooterComponent={<Text style={styles.footerText}>End of List</Text>}
         ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setRefreshing(true);
+          fetchData(30);
+          setRefreshing(false);
+        }}
 
       />
 
